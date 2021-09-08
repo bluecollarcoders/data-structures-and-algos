e91c47edca2d64acd2b14c68f29c9f5619f8a54f
Caleb Matteis <matteismusic@gmail.com>
2021-09-08 10:27:34 -0400

    commit

---
+++ validSubsequence.js
@@ -0,0 +1,14 @@
+function isValidSubsequence(array, sequence) {
+    let j = 0;
+      for(let i = 0; i < array.length; i++) {
+          if(array[i] === sequence.length) {
+          } 
+          return true;
+      }
+  
+      
+  }
+  
+  // Do not edit the line below.
+  exports.isValidSubsequence = isValidSubsequence;
+  
